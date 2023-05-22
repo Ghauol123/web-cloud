096f1b25ad7bad58718ff86af9c0c5dd8b456fbd
Ghauol123 <haiviet1503@gmail.com>
2023-05-22 17:10:05 +0700

    test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000.`);
+// const mongoose = require('mongoose');
+// const readline = require('readline');
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/test');
+//   console.log('MongoDB connected');
+
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+
+//   rl.question('Enter user name: ', async (name) => {
+//     rl.question('Enter user age: ', async (age) => {
+//       const User = mongoose.model('User', {
+//         name: String,
+//         age: Number
+//       });
+
+//       const user = new User({ name, age });
+
+//       try {
+//         await user.save();
+//         console.log('User inserted successfully:', user);
+//       } catch (error) {
+//         console.log('Error inserting user:', error);
+//       } finally {
+//         rl.close();
+//         mongoose.disconnect();
+//       }
+//     });
+//   });
+// }
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log('MongoDB connected');
+
+  const User = mongoose.model('User', {
+    name: String,
+    age: Number
   });
+
+  try {
+    const user = await User.find({});
+    console.log('Here is the record:');
+    console.log(user);
+  } catch (error) {
+    console.log('Error retrieving user:', error);
+  } finally {
+    mongoose.disconnect();
+  }
+}
