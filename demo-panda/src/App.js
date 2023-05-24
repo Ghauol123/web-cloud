00a3980c4aef447444da6b2a31d12b1f7fffbdbf
Ghauol123 <haiviet1503@gmail.com>
2023-05-24 13:33:56 +0700

    Update App.js

--- demo-panda/src/App.js
+++ demo-panda/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000/message")
+    fetch("https://web-server-2myj.onrender.com/message")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
